9e41c366a90c09485617f1bff1afb9e8f79605cc
Lucas Gregorio Martins <lucas.martins@tqi.com.br>
2021-01-22 13:04:54 -0300

    Adicionado busca por Id de Tipo conta

--- src/main/java/com/orangeBank/controller/TipoContaController.java
+++ src/main/java/com/orangeBank/controller/TipoContaController.java
@@ -2,9 +2,11 @@ package com.orangeBank.controller;
 
 import com.orangeBank.model.TipoConta;
 import com.orangeBank.service.TipoContaService;
+import com.sun.xml.bind.v2.model.core.ID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +22,12 @@ public class TipoContaController {
     @GetMapping("listar")
     public ResponseEntity<List<TipoConta>> findAll(){
         return ResponseEntity.ok(this.tipoContaService.findAll());
+    }
+
+    @GetMapping(value = "/{id_tipo_conta}")
+    public ResponseEntity<TipoConta> findById(@PathVariable Integer id_tipo_conta){
+        TipoConta tipoConta = tipoContaService.findById(id_tipo_conta);
+        return ResponseEntity.ok().body(tipoConta);
+
     }
 }
--- src/main/java/com/orangeBank/service/TipoContaService.java
+++ src/main/java/com/orangeBank/service/TipoContaService.java
@@ -4,10 +4,10 @@ import com.orangeBank.exception.ObjectNotFoundException;
 import com.orangeBank.model.TipoConta;
 import com.orangeBank.repository.TipoContaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TipoContaService {
@@ -18,7 +18,12 @@ public class TipoContaService {
     public List<TipoConta> findAll(){
         List<TipoConta> tipoConta = this.tipoContaRepository.findAll();
         return tipoConta;
-
     }
+
+    public TipoConta findById(Integer id_tipo_conta){
+        Optional<TipoConta> tipoConta = this.tipoContaRepository.findById(id_tipo_conta);
+        return tipoConta.orElseThrow(() -> new ObjectNotFoundException("Tipo de conta não encontrado! Id: "+id_tipo_conta));
+    }
+
 
 }
